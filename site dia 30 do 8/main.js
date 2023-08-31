03242886103525a5c8397c17058b767298ac1cf1
Jhowney <127686121+Jhowney@users.noreply.github.com>
2023-08-31 16:25:46 -0300

    Delete site dia 30 do 8 directory

--- site dia 30 do 8/main.js
+++
@@ -1,8 +0,0 @@
-//Pom ------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();   
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla');
-
-ListaDeTeclas[0].onclick = tocaSomPom;
