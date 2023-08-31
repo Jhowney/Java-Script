8884a7d3d16e2df813a2bd549d76ce92e8fe429e
Jhowney <127686121+Jhowney@users.noreply.github.com>
2023-08-31 16:26:20 -0300

    Add files via upload

---
+++ site alura 31 do 8/main.js
@@ -0,0 +1,40 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);   
+
+    if (elemento && elemento.LocalName === 'audio') {
+        elemento.play();
+        
+    }
+    else {
+        //aler('Elemento não encontrado')
+        console.log('Elemento não encontrado')
+         }        
+        
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+
+    const tecla = ListaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento) {
+        
+        if (evento.code === 'Space' || evento.code === 'Enter') {   
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
